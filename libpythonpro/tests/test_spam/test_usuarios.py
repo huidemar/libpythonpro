8b06b2d1487c321e1fac44450e38588a135b0c6d
huidemar <huidemar@gmail.com>
2020-05-10 20:35:57 -0300

    Criado um test para salvar e listar usuários

---
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -0,0 +1,20 @@
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='Foo')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
+
+def test_listar_usuarios():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Foo'),Usuario(nome='Ticio')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuario == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
