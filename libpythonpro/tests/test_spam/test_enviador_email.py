184beeb4639f4d1b660759fad91293f412fbdb8c
huidemar <huidemar@gmail.com>
2020-05-10 20:20:32 -0300

    Parametrização de testes

--- libpythonpro/tests/test_spam/test_enviador_email.py
+++ libpythonpro/tests/test_spam/test_enviador_email.py
@@ -1,13 +1,25 @@
+import pytest
+
 from libpythonpro.spam.enviador_email import Enviador
+
 
 def test_criar_enviador_email():
     enviador = Enviador()
     assert enviador is not None
 
-def test_remetente():
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['huidemar@gmail.com', 'foo@bar.com.br']
+)
+
+
+def test_remetente(remetente):
     enviador = Enviador()
-    resultado = enviador.enviar('huidemar@gmail.com',
-                    'luciano@python.pro.br',
-                    'Assunto - test Curso python PRo',
-                    'Turma Luis Vital - teste e-mail')
-    assert 'huidemar@gmail.com' in resultado
+    resultado = enviador.enviar(
+                                remetente,
+                                'luciano@python.pro.br',
+                                'Assunto - test Curso python PRo',
+                                'Turma Luis Vital - teste e-mail'
+                                )
+    assert remetente in resultado
