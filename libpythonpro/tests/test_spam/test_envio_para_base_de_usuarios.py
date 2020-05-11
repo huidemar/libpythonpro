b8b5f9da90ca5fc9bcccdfcf077d66d11f33e9ce
huidemar <huidemar@gmail.com>
2020-05-11 09:09:55 -0300

    Implementado Injeção de Dependências ()

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,7 +1,18 @@
 from libpythonpro.spam.enviador_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
-import  pytest
+import pytest
 from libpythonpro.spam.modelos.usuario import Usuario
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtde_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio(remetente, destinatario, assunto, corpo)
+        self.qtde_email_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -19,10 +30,27 @@ from libpythonpro.spam.modelos.usuario import Usuario
 def test_qtde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('foo@bar.com.br',
                                    'Curso Python Pro',
                                    'Confira os módulos fantásticos'
                                    )
     assert len(usuarios) == enviador.qtde_email_enviados
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Foo', email='foo@bar.com.br')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails('mevio@bar.com.br',
+                                   'Curso Python Pro',
+                                   'Confira os módulos fantásticos'
+                                   )
+    assert enviador.parametros_de_envio == (
+        'mevio@bar.com.br',
+        'foo@bar.combr'
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
