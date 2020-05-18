eefc19909322687edd98c1ca748c50c7720e4318
huidemar <huidemar@gmail.com>
2020-05-18 16:25:36 -0300

    Implementação de Imports

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,12 +1,24 @@
 from libpythonpro import github_api
 from unittest.mock import Mock
+import pytest
 
-def test_buscar_avatar():
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('huidemar')
+    assert avatar_url == url
 
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars3.githubusercontent.com/u/13460065?v=4'
     resp_mock.json.return_value = {
-        'avatar_url': 'https://avatars3.githubusercontent.com/u/13460065?v=4'
+        'avatar_url': url
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('huidemar')
     assert 'https://avatars3.githubusercontent.com/u/13460065?v=4' == url
