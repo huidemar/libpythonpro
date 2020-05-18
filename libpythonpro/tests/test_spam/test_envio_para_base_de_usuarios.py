d58744257ba4fd9d285ad1db5095c2e42a75efa6
huidemar <huidemar@gmail.com>
2020-05-18 15:48:43 -0300

    Implementação do Mock

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,18 +2,7 @@ from libpythonpro.spam.enviador_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 import pytest
 from libpythonpro.spam.modelos.usuario import Usuario
-
-
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qtde_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtde_email_enviados += 1
-
+from unittest.mock import Mock
 
 @pytest.mark.parametrize(
     'usuarios',
@@ -30,25 +19,25 @@ class EnviadorMock(Enviador):
 def test_qtde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('foo@bar.com.br',
                                    'Curso Python Pro',
                                    'Confira os módulos fantásticos'
                                    )
-    assert len(usuarios) == enviador.qtde_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Foo', email='foo@bar.com.br')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('mevio@bar.com.br',
                                    'Curso Python Pro',
                                    'Confira os módulos fantásticos'
                                    )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with == (
         'mevio@bar.com.br',
         'foo@bar.com.br',
         'Curso Python Pro',
